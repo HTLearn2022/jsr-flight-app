957d07baf3b9c0f5938889fbe1d42c34578eb243
HTLearn2022 <120314919+HTLearn2022@users.noreply.github.com>
2025-05-20 11:34:19 -0400

    Add files via upload

--- routes.py
+++ routes.py
@@ -1,7 +1,22 @@
-from flask import Blueprint, render_template
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
-main = Blueprint('main', __name__)
+socketio = SocketIO()
+db = SQLAlchemy()
+login_manager = LoginManager()
 
-@main.route("/")
-def home():
-    return render_template("index.html")
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'jsr123XELR&LJERSecurekey'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    db.init_app(app)
+    socketio.init_app(app)
+    login_manager.init_app(app)
+
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+
+    return app
--- run.py
+++ run.py
@@ -2,5 +2,5 @@ from routes import create_app, socketio
 
 app = create_app()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     socketio.run(app)
