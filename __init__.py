f83d9e0885cf936e8f8353a301190f3805bc65bc
HTLearn2022 <120314919+HTLearn2022@users.noreply.github.com>
2025-05-20 10:27:52 -0400

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_socketio import SocketIO
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+socketio = SocketIO()
+db = SQLAlchemy()
+login_manager = LoginManager()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'your_secret_key'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flights.db'
+    
+    db.init_app(app)
+    socketio.init_app(app)
+    login_manager.init_app(app)
+
+    from .routes import main
+    app.register_blueprint(main)
+
+    return app
