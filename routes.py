9f4a6470bb4e2e4ffb95aea7569404aab50c25de
HTLearn2022 <120314919+HTLearn2022@users.noreply.github.com>
2025-05-22 15:53:18 -0400

    routes.py

--- routes.py
+++ routes.py
@@ -1,16 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_socketio import SocketIO
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 socketio = SocketIO()
+db = SQLAlchemy()
+login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'jsr123XELR&LJERsecurekey'
+    app.config['SECRET_KEY'] = 'your_secret_key'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flights.db'
 
+    db.init_app(app)
     socketio.init_app(app)
+    login_manager.init_app(app)
 
-    @app.route("/")
-    def index():
-        return render_template("index.html")
+    from .routes import main
+    app.register_blueprint(main)
 
     return app
